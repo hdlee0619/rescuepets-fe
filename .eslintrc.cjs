1725b2d0c9784cae61536baa4168a96967abd6a6
hdlee0619 <dlgusehd1211@naver.com>
2023-08-30 22:13:43 +0900

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-shadow': 0,
+    'prettier/prettier': 'error',
+    'react/prop-types': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'react/function-component-definition': [
+      2,
+      { namedComponents: ['arrow-function', 'function-declaration'] },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'comma-dangle': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
