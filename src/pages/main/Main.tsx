0753df9536a9cc43b382677dad4a232131810273
hdlee0619 <dlgusehd1211@naver.com>
2023-09-03 21:09:56 +0900

    Feat | Lottie Animation 적용
    - 공공 데이터 fetch 중 Loading 시 적용할 Lottie 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,8 +1,12 @@
 import useFetchAbandonment from '@hooks/abandonment/useFetchAbandonment.tsx'
 import CardTable from '@components/common/card/CardTable.tsx'
+import DogLoading from '@components/common/loading/DogLoading.tsx'
 
 export default function Main() {
   const { abandonmentArray } = useFetchAbandonment([])
 
+  if (abandonmentArray.length === 0) {
+    return <DogLoading />
+  }
   return <CardTable dataArray={abandonmentArray} />
 }
