ecbf31893b21cd400dd7b8ebbc271609dd6a7f6b
hdlee0619 <dlgusehd1211@naver.com>
2023-09-05 21:57:13 +0900

    Feat | Mobile Logo, Nav bar 추가
    - 반응형 기능 추가

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,9 +1,11 @@
 import { Link, NavLink, Outlet } from 'react-router-dom'
-import { css } from 'styled-components'
+import { css, styled } from 'styled-components'
 import Container from '@components/elements/Container.tsx'
 import Stack from '@components/elements/Stack.tsx'
 import Divider from '@components/elements/Divider.tsx'
-import { ReactComponent as Logo } from '@assets/logo/logo.svg'
+import { ReactComponent as LaptopLogoSvg } from '@assets/logo/laptop_logo.svg'
+import { ReactComponent as MobileLogoSvg } from '@assets/logo/mobile_logo.svg'
+import { ReactComponent as Hamburger } from '@assets/svgicons/hamburger.svg'
 import { ReactComponent as HomeIcon } from '@assets/svgicons/home.svg'
 import { ReactComponent as PetworkIcon } from '@assets/svgicons/petwork.svg'
 import { ReactComponent as ChatIcon } from '@assets/svgicons/chat.svg'
@@ -14,42 +16,47 @@ export default function Header() {
     <>
       <Container $sx={HeaderContainerStyle}>
         <Link to="/">
-          <Logo />
+          <LaptopLogo />
+          <MobileLogo />
         </Link>
-        <Stack $sx={NavbarStyle}>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            <HomeIcon />
-            <span>Home</span>
-          </NavLink>
-          <NavLink
-            to="/petwork"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            <PetworkIcon />
-            <span>Petwork</span>
-          </NavLink>
-          <NavLink
-            to="/chat"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            <ChatIcon />
-            <span>Chat</span>
-          </NavLink>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            <ProfileIcon />
-            <span>Profile</span>
-          </NavLink>
-        </Stack>
+        <LaptopNavbar />
+        <HamburgerIcon />
       </Container>
       <Divider $density="2px" />
       <Outlet />
     </>
+  )
+}
+
+const LaptopNavbar = () => {
+  return (
+    <Stack $sx={NavbarStyle}>
+      <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+        <HomeIcon />
+        <span>Home</span>
+      </NavLink>
+      <NavLink
+        to="/petwork"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        <PetworkIcon />
+        <span>Petwork</span>
+      </NavLink>
+      <NavLink
+        to="/chat"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        <ChatIcon />
+        <span>Chat</span>
+      </NavLink>
+      <NavLink
+        to="/profile"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        <ProfileIcon />
+        <span>Profile</span>
+      </NavLink>
+    </Stack>
   )
 }
 
@@ -58,6 +65,31 @@ const HeaderContainerStyle = css`
   align-items: center;
   justify-content: space-between;
   padding: 8px 0 8px 0;
+`
+
+const LaptopLogo = styled(LaptopLogoSvg)`
+  @media (max-width: ${({ theme }) => theme.screen.md}) {
+    display: none;
+  }
+`
+
+const MobileLogo = styled(MobileLogoSvg)`
+  padding: 4px 0 4px 16px;
+  width: 38px;
+  height: 44px;
+  @media (min-width: ${({ theme }) => theme.screen.md}) {
+    display: none;
+  }
+`
+
+const HamburgerIcon = styled(Hamburger)`
+  padding: 4px 16px 4px 0;
+  width: 38px;
+  height: 44px;
+  cursor: pointer;
+  @media (min-width: ${({ theme }) => theme.screen.md}) {
+    display: none;
+  }
 `
 
 const NavbarStyle = css`
@@ -81,5 +113,9 @@ const NavbarStyle = css`
     circle {
       fill: ${({ theme }) => theme.color.primary.main};
     }
+  }
+
+  @media (max-width: ${({ theme }) => theme.screen.md}) {
+    display: none;
   }
 `
